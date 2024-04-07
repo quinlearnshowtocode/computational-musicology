5ca21d7120cc2a1403f22e6e85626219cf691672
Chloe <Chloe@Quin-9.local>
2024-04-08 01:32:36 +0200

    com

--- r/download-data.R
+++ r/download-data.R
@@ -10,13 +10,39 @@ es_analyzed <- es %>% mutate(playlist = "Latino Hiphop") %>% add_audio_analysis(
 
 
 cd <-
-  get_tidy_audio_analysis("19xVZr90ICYmatwd3FsKbe") 
+  get_tidy_audio_analysis("19xVZr90ICYmatwd3FsKbe")
 thot <-
   get_tidy_audio_analysis("1PoxR9dqTzlGYFJkHnpNfS") 
 como <-
-  get_tidy_audio_analysis("1PoxR9dqTzlGYFJkHnpNfS")
+  get_tidy_audio_analysis("76j6ISLdqxSKJBNGLL0fPb")
 tl <-
   get_tidy_audio_analysis("7gewLHcVTy1TrlwqSpuTJF") 
+
+
+hiphop <- bind_rows(fr_analyzed, de_analyzed, kr_analyzed, es_analyzed)
+
+
+hiphop_features <-
+  hiphop |>
+  mutate(playlist = factor(playlist)) |>
+  mutate(segments = map2(segments, key, compmus_c_transpose)) |>
+  mutate(
+    pitches =
+      map(
+        segments,
+        compmus_summarise, pitches,
+        method = "mean", norm = "manhattan"
+      ),
+    timbre =
+      map(
+        segments,
+        compmus_summarise, timbre,
+        method = "mean"
+      )
+  ) |>
+  mutate(pitches = map(pitches, compmus_normalise, "clr")) |>
+  mutate_at(vars(pitches, timbre), map, bind_rows) |>
+  unnest(cols = c(pitches, timbre))
 
 
 
@@ -27,7 +53,6 @@ saveRDS(object = thot, file = "data/thot.RDS")
 saveRDS(object = como, file = "data/como.RDS")
 
 saveRDS(object = tl, file = "data/tl.RDS")
-
 
 
 
@@ -47,4 +72,7 @@ saveRDS(object = es_analyzed, file = "data/es_all.RDS")
 saveRDS(object = kr_analyzed, file = "data/kr_all.RDS")
 
 saveRDS(object = de_analyzed, file = "data/de_all.RDS")
+
+
+saveRDS(object = hiphop_features, file = "data/hiphop_features.RDS")
 
